0208516552f46591651490fcb1bb653dd1203cf2
nabi_huang <nabi_huang@paradise-soft.com.tw>
2020-07-07 15:45:22 +0800

    456

--- setup.py
+++ setup.py
@@ -1,21 +1,17 @@
 import setuptools
 
-# with open("README.md", "r") as fh:
-#     long_description = fh.read()
-
 setuptools.setup(
     name="MAT",
     version="0.0.1",
-    packages=setuptools.find_packages('mat'),
+    packages=setuptools.find_packages(),
     install_requires=[
         'Click',
         'confuse',
         'requests',
         'flask',
-        'flask-cors',
     ],
-     entry_points={'console_scripts':[
-         'mat=app:cli',
-     ]},
-    python_requires='>=3.6',
+     entry_points='''
+        [console_scripts]
+        mat=app:cli
+    ''',
 )

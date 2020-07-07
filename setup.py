e078fab3fc54db14f542eee1dc66f8ecf2cdf2ba
Nabi <40596572+nabi0225@users.noreply.github.com>
2020-07-07 12:05:12 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ import setuptools
 setuptools.setup(
     name="MAT",
     version="0.0.1",
-    packages=find_packages(),
+    packages=setuptools.find_packages(),
     install_requires=[
         'Click',
         'confuse',

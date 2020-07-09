38b9fe46f1e023175922cc6be9d4a21687301558
Nabi <40596572+nabi0225@users.noreply.github.com>
2020-07-09 22:17:06 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,6 @@ setuptools.setup(
     version="0.0.1",
     packages=setuptools.find_packages(),
     include_package_data=True,
-    package_dir = {'': 'data'},
     install_requires=[
         'Click',
         'confuse',

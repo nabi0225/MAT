1c498b9b80040f9095ad6c36789d7ad01c77ac5d
Nabi <40596572+nabi0225@users.noreply.github.com>
2020-07-09 18:07:11 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setuptools.setup(
     name="MAT",
     version="0.0.1",
     url='https://github.com/nabi0225/MAT.git',
-    packages=setuptools.find_packages('MAT'),
+    packages=setuptools.find_packages('mat'),
     include_package_data=True,
     py_modules=['mat'],
     install_requires=[

5a8835792a6cde33e024b2c01bb9a63502607c14
Nabi <40596572+nabi0225@users.noreply.github.com>
2020-07-09 19:08:39 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 setuptools.setup(
     name="MAT",
     version="0.0.1",
-    packages=setuptools.find_packages(),
+    packages=setuptools.find_packages(exclude=['config.yaml', 'data']),
     package_data={'mat': ['config.yaml', 'data']},
     include_package_data=True,
     install_requires=[

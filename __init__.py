39c006b414be160f76e1e79aea7017e72ddd4c7d
Nabi <40596572+nabi0225@users.noreply.github.com>
2020-07-09 17:06:49 +0800

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,13 +1,1 @@
-import json
-import shutil
 
-import confuse as confuse
-import requests
-from flask import Flask, request, jsonify, Response
-from flask_cors import CORS
-import click
-import yaml
-import os
-
-app = Flask(__name__)
-CORS(app)

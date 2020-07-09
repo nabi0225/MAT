23f49689adb674ef71dc8a99a9817f2442cde9ee
Nabi <40596572+nabi0225@users.noreply.github.com>
2020-07-09 18:29:49 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 setuptools.setup(
     name="MAT",
     version="0.0.1",
-    packages=setuptools.find_packages('mat'),
+    packages=setuptools.find_packages(),
     include_package_data=True,
     install_requires=[
         'Click',

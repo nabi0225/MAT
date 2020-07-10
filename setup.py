42cfdf14d6af54106ee3dcf257270f2fdd0c6afc
nabi_huang <nabi_huang@paradise-soft.com.tw>
2020-07-10 15:01:08 +0800

    update setup

--- setup.py
+++ setup.py
@@ -4,8 +4,10 @@ setuptools.setup(
     name="MAT",
     version="0.0.1",
     packages=setuptools.find_packages(),
-    package_dir={'': 'mat/config.yaml'},
-    package_data={'': ['mat/data/*.json']},
+    package_data={
+        'data': ['mat/data/*.json'],
+        'config': ['mat/config.yaml'],
+    },
     install_requires=[
         'Click',
         'confuse',
